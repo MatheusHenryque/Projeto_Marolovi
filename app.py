c1ddfa5c910d408d249e21069bfcc29eecbeb2e8
MatheusHenryque <mth123922@gmail.com>
2025-09-04 16:13:27 -0300

    Atualização de sidebar para bottom bar no mobile

--- app.py
+++ app.py
@@ -156,7 +156,4 @@ def recursos():
 
 
 if __name__ == '__main__':
-    # Obtém a porta da variável de ambiente ou usa 5000 como padrão
-    port = int(os.environ.get("PORT", 5000))
-    # Executa a aplicação
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=int(os.getenv("PORT", 5000)))
