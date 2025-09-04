81ccb6b0f62556e0fca3d25b63381de4ff35f943
MatheusHenryque <mth123922@gmail.com>
2025-09-04 16:16:27 -0300

    merge de mudanças

--- app.py
+++ app.py
@@ -156,4 +156,4 @@ def recursos():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.getenv("PORT", 5000)))
+    app.run(host='0.0.0.0',debug=True, port=int(os.getenv("PORT", 5000)))
