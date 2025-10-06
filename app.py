99af7b27f320ad01e20e7118d5ea066a6fecf758
vitor <vitornones25@gmail.com>
2025-10-06 14:58:49 -0300

    Feat/ChatbotHardcoded

--- app.py
+++ app.py
@@ -5,6 +5,8 @@ import numpy as np
 from PIL import Image
 import onnxruntime as ort 
 import secrets
+import random
+import json
 
 
 app = Flask(__name__)
@@ -165,18 +167,22 @@ def recursos():
 def chat():
 
     data = request.get_json()
-
     if not data or 'mensagem' not in data:
         return jsonify({"error": "Nenhuma mensagem recebida."}), 400
         
-    if data['mensagem'].lower() == "colirios" or data['mensagem'].lower() == "quais colírios vocês recomendam?" or data['mensagem'].lower() == "quais colírios vocês recomendam":
-        return jsonify({"resposta": "colírios lubrificantes: Lacrifilm, Systane, Optive, Blink, Refresh"})   
-    
-    user_message = data['mensagem']
+    user_message = data['mensagem'].lower()
+
+    with io.open('static/chatbotHardCoded/chatbot.json', 'r', encoding='utf-8') as f:
+        intents = json.load(f)
+        
+    for intent in intents['intents']:
+        for pattern in intent['patterns']:
+            if pattern.lower() in user_message:
+                bot_response = random.choice(intent['responses'])
+                return jsonify({"resposta": bot_response})
 
     bot_response = f"OFTBOT não reconhece: '{user_message}'"
-
     return jsonify({"resposta": bot_response})
-
+    
 if __name__ == '__main__':
     app.run(host='0.0.0.0',debug=True, port=int(os.getenv("PORT", 5000)))
